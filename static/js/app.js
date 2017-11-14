de3c5fafa5a246dc0bea402f6e732a6f2627db62
OJ04 <ville.ojaniemi@elisanet.fi>
2017-11-14 12:19:07 +0200

    Data fetched from backend and updated using AJAX call

--- static/js/app.js
+++ static/js/app.js
@@ -14,8 +14,25 @@ svg.append("g")
 	.attr("class", "slices");
 svg.append("g")
 	.attr("class", "trees");
-ClassesData=[{"name":"Presenter","id":"0","var":0,"color":"#98abc5","value":1,"url":"/resources/images/presentations/0.png","children":[{"name":"1st Presentation","id":"0.0","var":0,"url":"/resources/images/presentations/00.png","children":[{"name":"Bilingual","id":"0.0.0","var":0,"url":"/resources/images/presentations/000.png",},{"name":"Group","id":"0.0.1","var":0,"url":"/resources/images/presentations/001.png","children":[{"name":"Crowd","id":"0.0.1.0","url":"/resources/images/presentations/0010.png","var":0}]}]},{"name":"1stSlides","id":"0.1","var":0,"url":"/resources/images/presentations/01.png","children":[{"name":"PowerPoint","id":"0.1.0","var":0,"url":"/resources/images/presentations/010.jpg","children":[{"name":"Keynote","id":"0.1.0.0","url":"/resources/images/presentations/0100.png","var":0}]},{"name":"Animations","id":"0.1.1","var":0,"url":"/resources/images/presentations/011.png","children":[{"name":"Videos","id":"0.1.1.0","url":"/resources/images/presentations/0110.png","var":0,"children":[{"name":"DemoPlay","id":"0.1.1.0.0","url":"/resources/images/presentations/01100.png","var":0}]}]}]}]},{"name":"C++Programer","id":"1","var":0,"color":"#aaaaaa","url":"/resources/images/c_plus_plus/1.png","value":1,"children":[{"name":"HelloWorld","id":"1.0","url":"/resources/images/c_plus_plus/10.png","var":0,"children":[{"name":"Application","id":"1.0.0","var":0,"url":"/resources/images/c_plus_plus/100.png","children":[{"name":"500loc","id":"1.0.0.0","url":"/resources/images/c_plus_plus/1000.png","var":0,"children":[{"name":"5000loc","id":"1.0.0.0.0","url":"/resources/images/c_plus_plus/10000.png","var":0,"children":[{"name":"50000loc","id":"1.0.0.0.0.0","url":"/resources/images/c_plus_plus/100000.png","var":0}]}]},{"name":"Debugging","id":"1.0.0.1","url":"/resources/images/c_plus_plus/1001.png","var":0,"children":[{"name":"UnitTesting","id":"1.0.0.1.0","url":"/resources/images/c_plus_plus/10010.png","var":0}]}]},{"name":"SyntaxTutorial","id":"1.0.1","url":"/resources/images/c_plus_plus/101.png","var":0,"children":[{"name":"Constructor","id":"1.0.1.0","url":"/resources/images/c_plus_plus/1010.png","var":0,"children":[{"name":"Pointer","id":"1.0.1.0.0","url":"/resources/images/c_plus_plus/10100.png","var":0,"children":[{"name":"TemplateProgramming","id":"1.0.1.0.0.0","url":"/resources/images/c_plus_plus/101000.png","var":0}]}]}]}]}]}];
-var userData = ["0","0.0","0.0.0","1","2"];
+//ClassesData=[{"name":"Presenter","id":"0","var":0,"color":"#98abc5","value":1,"url":"/resources/images/presentations/0.png","children":[{"name":"1st Presentation","id":"0.0","var":0,"url":"/resources/images/presentations/00.png","children":[{"name":"Bilingual","id":"0.0.0","var":0,"url":"/resources/images/presentations/000.png",},{"name":"Group","id":"0.0.1","var":0,"url":"/resources/images/presentations/001.png","children":[{"name":"Crowd","id":"0.0.1.0","url":"/resources/images/presentations/0010.png","var":0}]}]},{"name":"1stSlides","id":"0.1","var":0,"url":"/resources/images/presentations/01.png","children":[{"name":"PowerPoint","id":"0.1.0","var":0,"url":"/resources/images/presentations/010.jpg","children":[{"name":"Keynote","id":"0.1.0.0","url":"/resources/images/presentations/0100.png","var":0}]},{"name":"Animations","id":"0.1.1","var":0,"url":"/resources/images/presentations/011.png","children":[{"name":"Videos","id":"0.1.1.0","url":"/resources/images/presentations/0110.png","var":0,"children":[{"name":"DemoPlay","id":"0.1.1.0.0","url":"/resources/images/presentations/01100.png","var":0}]}]}]}]},{"name":"C++Programer","id":"1","var":0,"color":"#aaaaaa","url":"/resources/images/c_plus_plus/1.png","value":1,"children":[{"name":"HelloWorld","id":"1.0","url":"/resources/images/c_plus_plus/10.png","var":0,"children":[{"name":"Application","id":"1.0.0","var":0,"url":"/resources/images/c_plus_plus/100.png","children":[{"name":"500loc","id":"1.0.0.0","url":"/resources/images/c_plus_plus/1000.png","var":0,"children":[{"name":"5000loc","id":"1.0.0.0.0","url":"/resources/images/c_plus_plus/10000.png","var":0,"children":[{"name":"50000loc","id":"1.0.0.0.0.0","url":"/resources/images/c_plus_plus/100000.png","var":0}]}]},{"name":"Debugging","id":"1.0.0.1","url":"/resources/images/c_plus_plus/1001.png","var":0,"children":[{"name":"UnitTesting","id":"1.0.0.1.0","url":"/resources/images/c_plus_plus/10010.png","var":0}]}]},{"name":"SyntaxTutorial","id":"1.0.1","url":"/resources/images/c_plus_plus/101.png","var":0,"children":[{"name":"Constructor","id":"1.0.1.0","url":"/resources/images/c_plus_plus/1010.png","var":0,"children":[{"name":"Pointer","id":"1.0.1.0.0","url":"/resources/images/c_plus_plus/10100.png","var":0,"children":[{"name":"TemplateProgramming","id":"1.0.1.0.0.0","url":"/resources/images/c_plus_plus/101000.png","var":0}]}]}]}]}]}];
+var ClassesData = [];
+
+for(var i=0;i<content.data.length;i++)
+{
+	ClassesData.push(JSON.parse(content.data[0].content.replace(/'/g ,"\"")));
+}
+
+console.log(ClassesData);
+
+//var userData = ["0","0.0","0.0.0","1","2"];
+var savestate = content.saved_state;
+
+var userData = savestate.replace('[','').replace(']','').replace(/'/g ,'').split(',');
+
+
+
+
+
 var focusedID = null;
 
 var width = parseInt(d3.select("#class-diagram").select("svg").style("width"), 10);
@@ -198,12 +215,9 @@ function updateClassesData(id)
 					str = str.concat("'",udata[i],"'");
 				}
 			}
-			str.concat("]");
+			str = str.concat("]");
 			console.log(str);
-			
-			//you shall not PAASSSSSS!
 			return;
-			
 			console.log("Ajax POST");
 			//save to DB
 			$.ajax({
