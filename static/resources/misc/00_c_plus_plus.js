511d646b7430bb8f9fcc63b9e7342522c95761cd
DESKTOP-S3A4G9D\Daniel <DaZibion@gmail.com>
2017-11-21 23:04:35 +0200

    Add test data

---
+++ static/resources/misc/00_c_plus_plus.js
@@ -0,0 +1,76 @@
+{
+    "name": "C++ Programming",
+    "id": "1",
+    "var": 0,
+    "color": "#aaaaaa",
+    "url": "/resources/images/c_plus_plus/1.png",
+    "value": 1,
+    "children": [{
+        "name": "Hello World",
+        "id": "1.0",
+        "url": "/resources/images/c_plus_plus/10.png",
+        "var": 0,
+        "children": [{
+                "name": "Application",
+                "id": "1.0.0",
+                "var": 0,
+                "url": "/resources/images/c_plus_plus/100.png",
+                "children": [{
+                        "name": "500 loc",
+                        "id": "1.0.0.0",
+                        "url": "/resources/images/c_plus_plus/1000.png",
+                        "var": 0,
+                        "children": [{
+                            "name": "5000 loc",
+                            "id": "1.0.0.0.0",
+                            "url": "/resources/images/c_plus_plus/10000.png",
+                            "var": 0,
+                            "children": [{
+                                "name": "50000 loc",
+                                "id": "1.0.0.0.0.0",
+                                "url": "/resources/images/c_plus_plus/100000.png",
+                                "var": 0
+                            }]
+                        }]
+                    },
+                    {
+                        "name": "Debugging",
+                        "id": "1.0.0.1",
+                        "url": "/resources/images/c_plus_plus/1001.png",
+                        "var": 0,
+                        "children": [{
+                            "name": "Unit Testing",
+                            "id": "1.0.0.1.0",
+                            "url": "/resources/images/c_plus_plus/10010.png",
+                            "var": 0
+                        }]
+                    }
+                ]
+            },
+            {
+                "name": "Syntax Tutorial",
+                "id": "1.0.1",
+                "url": "/resources/images/c_plus_plus/101.png",
+                "var": 0,
+                "children": [{
+                    "name": "Constructor",
+                    "id": "1.0.1.0",
+                    "url": "/resources/images/c_plus_plus/1010.png",
+                    "var": 0,
+                    "children": [{
+                        "name": "Pointer",
+                        "id": "1.0.1.0.0",
+                        "url": "/resources/images/c_plus_plus/10100.png",
+                        "var": 0,
+                        "children": [{
+                            "name": "Template Programming",
+                            "id": "1.0.1.0.0.0",
+                            "url": "/resources/images/c_plus_plus/101000.png",
+                            "var": 0
+                        }]
+                    }]
+                }]
+            }
+        ]
+    }]
+}
---
+++ static/resources/misc/04_german.js
@@ -0,0 +1,47 @@
+{
+    'name': 'German',
+    'id': '4',
+    'var': 0,
+    'color': '#98abc5',
+    'value': 1,
+    'url': '/resources/images/german/4.png',
+    'children': [{
+            'name': 'Wurst',
+            'id': '4.0',
+            'var': 0,
+            'url': '/resources/images/german/40.png',
+            'children': [{
+                    'name': 'Speck',
+                    'id': '4.0.0',
+                    'var': 0,
+                    'url': '/resources/images/german/400.png'
+                },
+                {
+                    'name': 'Eier',
+                    'id': '4.0.1',
+                    'var': 0,
+                    'url': '/resources/images/german/401.png'
+                }
+            ]
+        },
+        {
+            'name': 'Successful Project',
+            'id': '4.1',
+            'var': 0,
+            'url': '/resources/images/german/41.png',
+            'children': [{
+                    'name': 'Kraut',
+                    'id': '4.1.0',
+                    'var': 0,
+                    'url': '/resources/images/german/410.png'
+                },
+                {
+                    'name': 'Apfel',
+                    'id': '4.1.1',
+                    'var': 0,
+                    'url': '/resources/images/german/411.png'
+                }
+            ]
+        }
+    ]
+}
